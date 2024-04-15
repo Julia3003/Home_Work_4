44668272799b4364c28e604bf7fb9a7d08bc2ce9
Julia Korchevskaja <yourii.korchevskii@forforce.com>
2024-04-15 21:52:22 +0300

    Home Work 5

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-
+//Обчислення площі кола
 //First variation - функція повертає нове число
 function areaCalculation(int|float $radius):int|float
 {
@@ -19,6 +19,7 @@ areaCalculationSecond($numberRadius2);
 echo $numberRadius2 . PHP_EOL;
 
 
+// Приймає число і підносить його до ступеню - з наявністю аргумента з дефолтним значенням - це $power
 //First variation - функція повертає нове число
 function power(int|float $number, int $power = 2):int|float
 {
@@ -29,7 +30,7 @@ $myNumber = 5;
 $powerResult = power($myNumber);
 echo $powerResult . PHP_EOL;
 
-//Second variation - функція змінює передане число
+//Second variation - функція змінює передане число - з наявністю аргумента з дефолтним значенням - це $power
 function powerSecond(int|float &$number, int $power = 2): void
 {
 	$number = $number ** $power;
@@ -38,3 +39,27 @@ function powerSecond(int|float &$number, int $power = 2): void
 $myNumber2 = 7;
 powerSecond($myNumber2);
 echo $myNumber2 . PHP_EOL;
+
+
+// Приймає число і підносить його до ступеню - всі аргументи обов'язкові
+//First variation - функція повертає нове число
+function powerVarSecond(int|float $number, int $power):int|float
+{
+	return $number ** $power;
+}
+
+$myNumber = 5;
+$degree = 3;
+$powerResult = power($myNumber, $degree);
+echo $powerResult . PHP_EOL;
+
+//Second variation - всі аргументи обов'язкові
+function powerVarSeconds(int|float &$number, int $power): void
+{
+	$number = $number ** $power;
+}
+
+$myNumberSecond = 7;
+$degree2 = 2;
+powerSecond($myNumberSecond, $degree2);
+echo $myNumberSecond . PHP_EOL;
