3fc35648a4a01d037deb6e84923a5365ac1a6d88
Julia Korchevskaja <yourii.korchevskii@forforce.com>
2024-04-15 23:17:44 +0300

    Home Work 6

--- src/index.php
+++ src/index.php
@@ -1,65 +1,22 @@
 <?php
 
-// First option (the most optimal)
-$value = 1;
-$color = match($value) {
-    1 => 'green',
-    2 => 'red',
-    3 => 'blue',
-    4 => 'brown',
-    5 => 'violet',
-    6 => 'black',
-    default => 'white'
+function Calculation(int $number1, int $number2, ?closure $callback = null)
+{
+	$multiplication = $number1 * $number2;
+	if (null !== $callback){
+		return $callback($multiplication);
+	}
+	return $multiplication;
+}
+
+$val1 = 15;
+$val2 = 10;
+$result =  Calculation($val1, $val1);
+
+$function = function (int $result){
+	echo $result . PHP_EOL;
 };
-echo $color . PHP_EOL;
 
-
-
-// Second option
-$value = 10;
-switch ($value) {
-	case 1:
-	    $color = 'green';
-	    break;
-	case 2:
-	    $color = 'red';
-	    break;
-	case 3:
-		$color = 'blue';
-		break;
-	case 4:
-		$color = 'brown';
-		break;
-	case 5:
-		$color = 'violet';
-	    break;
-	case 6:
-		$color = 'black';
-	    break;
-	default:
-		$color = 'white';
-}
-echo $color . PHP_EOL;
-
-
-// Third option
-$value = 4;
-if ($value === 1){
-	$color = 'green';
-}elseif ($value === 2) {
-	$color = 'red';
-}elseif ($value === 3) {
-	$color = 'blue';
-}elseif ($value === 4) {
-	$color = 'brown';
-}elseif ($value === 5){
-	$color = 'violet';
-}elseif ($value === 6) {
-	$color = 'black';
-}else{
-	$color = 'white';
-}
-echo $color . PHP_EOL;
-
-
-
+//$function();
+//
+//$function = 'calculate';
