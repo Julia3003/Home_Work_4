3fccbd061753c23c07464ba5047d680978b5fb62
Julia Korchevskaja <yourii.korchevskii@forforce.com>
2024-04-18 23:21:39 +0300

    Home Work 7

--- src/index.php
+++ src/index.php
@@ -1,18 +1,21 @@
 <?php
-declare(strict_types=1);
 
-//$i = 1;
-//while ($i <= 10){
-//	echo $i++ . PHP_EOL;
-//}
-//
-//
-//$i = 1;
-//$result = 1;
-//while ($i <= 5){
-//	$result *= $i++;
-//}
-//echo $result . PHP_EOL;
+$i = 1;
+while ($i <= 10) {
+	echo $i . PHP_EOL;
+	$i++;
+}
+
+
+
+$i = 1;
+$result = 1;
+while ($i <= 5) {
+	$result *= $i++;
+}
+echo $result . PHP_EOL;
+
+
 
 
 $i = 1;
