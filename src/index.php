37cfa7b1a745deb10ae004b0e8ac23ed249d858e
Julia Korchevskaja <yourii.korchevskii@forforce.com>
2024-04-21 20:50:08 +0300

    Home Work 9

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types=1);
-
+//1й варіант
 function generateArray(int $length = 10, int $min = 1, int $max = 10): array
 {
 	$array = [];
@@ -18,27 +18,22 @@ $myArray = generateArray();
 var_dump($myArray);
 
 
-//function generateArray2(int $length = 10, int $min = 1, int $max = 10, $callback): array
-//{
-//	$array2 = [];
-//	for ($i = 0; $i < $length; $i++) {
-//		$array2[] = rand($min, $max);
-//		$callback = function(int $array2[$i], int $array2[$i++]) int|mixed {
-//			return $array2[$i] <=> $array2[$i++];
-//		}
-//	}
-//
-//	usort($array2, $callback);
-//	return $array2;
-//};
-//
-//	print_r($array2);
-//
-//$myArray2 = generateArray2($callback);
-//var_dump($myArray2);
-//$callback = function(array $myArray2) {
-//	return $myArray2[$i] <=> $myArray2[$i++];
-//};
+//2й варіант
+function generateArray2(closure $callback, int $length = 10, int $min = 1, int $max = 10): array
+{
+	$array = [];
+	for ($i = 0; $i < $length; $i++) {
+		$array[] = rand($min, $max);
+	}
+	echo max($array). PHP_EOL;
+	echo min($array) . PHP_EOL;
+	usort($array, $callback);
+	return $array;
+}
 
 
-
+$callback = function(int $a, int $b) {
+	return $a <=> $b;
+};
+$myArray2 = generateArray2($callback);
+print_r($myArray2);
