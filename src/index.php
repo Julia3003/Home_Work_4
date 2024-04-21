571034426bcaa45ee3bf46b0dcddf521be0bcf05
Julia Korchevskaja <yourii.korchevskii@forforce.com>
2024-04-21 18:25:43 +0300

    Home Work 9

--- src/index.php
+++ src/index.php
@@ -1,65 +1,19 @@
 <?php
 
-// First option (the most optimal)
-$value = 1;
-$color = match($value) {
-    1 => 'green',
-    2 => 'red',
-    3 => 'blue',
-    4 => 'brown',
-    5 => 'violet',
-    6 => 'black',
-    default => 'white'
-};
-echo $color . PHP_EOL;
+declare(strict_types=1);
 
-
-
-// Second option
-$value = 10;
-switch ($value) {
-	case 1:
-	    $color = 'green';
-	    break;
-	case 2:
-	    $color = 'red';
-	    break;
-	case 3:
-		$color = 'blue';
-		break;
-	case 4:
-		$color = 'brown';
-		break;
-	case 5:
-		$color = 'violet';
-	    break;
-	case 6:
-		$color = 'black';
-	    break;
-	default:
-		$color = 'white';
+function generateArray(int $length = 10, int $min = 1, int $max = 10): array
+{
+	$array = [];
+	for ($i = 0; $i < $length; $i++) {
+		$array[] = rand($min, $max);
+	}
+	echo max($array). PHP_EOL;
+	echo min($array) . PHP_EOL;
+	rsort($array);
+	return $array;
 }
-echo $color . PHP_EOL;
 
-
-// Third option
-$value = 4;
-if ($value === 1){
-	$color = 'green';
-}elseif ($value === 2) {
-	$color = 'red';
-}elseif ($value === 3) {
-	$color = 'blue';
-}elseif ($value === 4) {
-	$color = 'brown';
-}elseif ($value === 5){
-	$color = 'violet';
-}elseif ($value === 6) {
-	$color = 'black';
-}else{
-	$color = 'white';
-}
-echo $color . PHP_EOL;
-
-
+$myArray = generateArray();
+var_dump($myArray);
 
