df8d4ca96750102784cb4f5f1de4ad137c3499e1
Julia Korchevskaja <yourii.korchevskii@forforce.com>
2024-04-21 23:31:03 +0300

    Home Work 10

--- src/index.php
+++ src/index.php
@@ -1,65 +1,99 @@
 <?php
+//$result = ['200', 'OK', 245];
+//list($status, $num1, $num2) = $result;
+//
+//
+//[, , $num2] = $result;
+//echo $num2 . PHP_EOL;
+//
+//$array = ['name' => 'Jim', 'age' => 20];
+//extract($array);
+//echo $name . PHP_EOL;
+//
+//$name = 'Jim';
+//$age = 20;
+//$array = compact('name', 'age');
+//print_r($array);
+//
+//
+//$array = [1,2,3,4,5,6,7];
+//foreach ();
 
-// First option (the most optimal)
-$value = 1;
-$color = match($value) {
-    1 => 'green',
-    2 => 'red',
-    3 => 'blue',
-    4 => 'brown',
-    5 => 'violet',
-    6 => 'black',
-    default => 'white'
-};
-echo $color . PHP_EOL;
+//function generatorArray(int $length = 10, int $min = 1, int $max = 10) : array
+//{
+//	$array = [];
+//	for ($i = 0; $i < $length; $i++) {
+//		$array[] = rand($min, $max);
+//	}
+//	return $array;
+//}
+//
+//$array = generatorArray();
+//
+//foreach ($array as $value) {
+//	echo $value . PHP_EOL;
+//}
 
 
 
-// Second option
-$value = 10;
-switch ($value) {
-	case 1:
-	    $color = 'green';
-	    break;
-	case 2:
-	    $color = 'red';
-	    break;
-	case 3:
-		$color = 'blue';
-		break;
-	case 4:
-		$color = 'brown';
-		break;
-	case 5:
-		$color = 'violet';
-	    break;
-	case 6:
-		$color = 'black';
-	    break;
-	default:
-		$color = 'white';
+function generator(int $length = 10, int $min = 1, int $max = 10) : Generator
+{
+	for ($i = 0; $i < $length; $i++) {
+		yield rand($min, $max);
+	}
 }
-echo $color . PHP_EOL;
 
+$generator = generator();
 
-// Third option
-$value = 4;
-if ($value === 1){
-	$color = 'green';
-}elseif ($value === 2) {
-	$color = 'red';
-}elseif ($value === 3) {
-	$color = 'blue';
-}elseif ($value === 4) {
-	$color = 'brown';
-}elseif ($value === 5){
-	$color = 'violet';
-}elseif ($value === 6) {
-	$color = 'black';
-}else{
-	$color = 'white';
+foreach ($generator as $value) {
+	echo $value . PHP_EOL;
 }
-echo $color . PHP_EOL;
 
 
+//$array = generatorArray();
+//
+//foreach ($array as $value) {
+//	echo $value . PHP_EOL;
+//}
 
+//function generator (): Generator
+//{
+//	yield 1;
+//	yield 5;
+//}
+//
+//$generator = generator();
+//
+//foreach ($generator as $value) {
+//	echo $value . PHP_EOL;
+//}
+//
+//var_dump($generator);
+
+//function myTest() : Generator
+//{
+//  yield from [5,6,7,8,9];
+//}
+//
+//foreach (myTest() as $value) {
+//	echo $value . PHP_EOL;
+///
+///
+///
+
+function fibanachi ($number){
+	yield rand ($min, $max);
+}
+
+function generator(int $length = 10, int $min = 1, int $max = 10) : Generator
+{
+	for ($i = 0; $i < $length; $i++) {
+		yield rand($min, $max);
+	}
+}
+
+$generator = generator();
+
+foreach ($generator as $value) {
+	echo $value . PHP_EOL;
+}
