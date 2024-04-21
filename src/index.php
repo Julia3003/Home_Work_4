c84f461a2061bcff6b2fb3c80e079c1557e694c8
Julia Korchevskaja <yourii.korchevskii@forforce.com>
2024-04-21 19:04:27 +0300

    Home Work 9

--- src/index.php
+++ src/index.php
@@ -17,3 +17,28 @@ function generateArray(int $length = 10, int $min = 1, int $max = 10): array
 $myArray = generateArray();
 var_dump($myArray);
 
+
+//function generateArray2(int $length = 10, int $min = 1, int $max = 10, $callback): array
+//{
+//	$array2 = [];
+//	for ($i = 0; $i < $length; $i++) {
+//		$array2[] = rand($min, $max);
+//		$callback = function(int $array2[$i], int $array2[$i++]) int|mixed {
+//			return $array2[$i] <=> $array2[$i++];
+//		}
+//	}
+//
+//	usort($array2, $callback);
+//	return $array2;
+//};
+//
+//	print_r($array2);
+//
+//$myArray2 = generateArray2($callback);
+//var_dump($myArray2);
+//$callback = function(array $myArray2) {
+//	return $myArray2[$i] <=> $myArray2[$i++];
+//};
+
+
+
