9d0989abe8fe2d314baa4f151b0b7707a9f6c47d
Julia Korchevskaja <yourii.korchevskii@forforce.com>
2024-04-24 21:46:38 +0300

    Home Work 11

--- src/index.php
+++ src/index.php
@@ -1,13 +1,59 @@
 <?php
 
-include 'function.php';
+//$path = '../info.txt';
+//
+//if (!file_exists($path)) {
+//	return;
+//}
+//
+//$filesize = filesize($path);
+//$file = fopen($path, 'r');
+//$content = [];
+//
+//echo fgets($file);
+//while (($line = fgets($file)) !== false) {
+//	echo $content = trim($line);
+//}
 
-showMessage('test');
+//$text = fread($file, $filesize);
+//
+//fclose($file);
+//
+//print_r($content);
 
-echo __DIR__ . PHP_EOL;
+//include 'function.php';
+//
+//showMessage('test');
+//
+//echo __DIR__ . PHP_EOL;
+//
+////include '../function/function.php';
+//
+//echo __FILE__ . PHP_EOL;
+//
+//print_r(get_included_files());
 
-//include '../function/function.php';
+$path = '../info.txt';
+$file = fopen($path, 'w');
 
-echo __FILE__ . PHP_EOL;
+if (!file_exists($path)) {
+	echo 'Файла не існує';
+	return;
+}
 
-print_r(get_included_files());
+echo 'Введіть аргументи в консоль';
+$content = [];
+while ($getConsole = fgets(STDIN) !== false) {
+	fwrite($file, $getConsole );
+}
+fclose($file);
+
+//echo $getConsole;
+//$filesize = filesize($path);
+//$file = fopen($path, 'w');
+//$getConsole = fgets(STDIN);
+//echo $getConsole;
+//$content = [];
+
+fclose($file);
+var_dump($content);
