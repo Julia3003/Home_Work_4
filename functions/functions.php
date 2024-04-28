6f857f65fcec2f50f73a4747bd9d065c8a7ada61
Julia Korchevskaja <yourii.korchevskii@forforce.com>
2024-04-28 20:27:15 +0300

    Home_work_11_№2

--- functions/functions.php
+++ functions/functions.php
@@ -1,33 +1,17 @@
 <?php
-// 1й варіант Програма, яка приймає з консолі аргументи, які введені, і записує їх в файл
+function getConsole(): string
+{
+	return trim(fgets(STDIN));
+}
 
-function getConsole1(string $path): bool
+function writeFile (string $path, string $allGetConsole): bool
 {
 	$file = fopen($path, 'a');
-	if (!file_exists($path)) {
-		return false;
-	}
-	$resultWrite = false;
-	while (($getConsole = trim(fgets(STDIN))) != false) {
-		$resultWrite = fwrite($file, $getConsole . PHP_EOL);
-	}
+	$resultWrite = fwrite($file, $allGetConsole . PHP_EOL);
 	fclose($file);
 	return (bool) $resultWrite;
 }
 
-//2й варіант Програма, яка приймає з консолі аргументи, які введені, і записує їх в файл
-function getConsole2(string $fileName): bool
-{
-	$getConsole = trim(fgets(STDIN));
-	$result = false;
-	if ($getConsole) {
-		$result = file_put_contents($fileName, $getConsole . PHP_EOL, FILE_APPEND);
-	}
-	return (bool) $result;
-}
-
-
-// 1й варіант Програма, яка виводить з файлу логу останні аргументи попередньої програми, які були введені.
 function fileReader(string $filePath): Generator|false
 {
 	if (!file_exists($filePath)) {
@@ -38,17 +22,4 @@ function fileReader(string $filePath): Generator|false
 		yield $line;
 	}
 	fclose($file);
-}
-
-// 2й варіант Програма, яка виводить з файлу логу останні аргументи попередньої програми, які були введені.
-function fileReade2(string $filePath): array|false
-{
-	if (!file_exists($filePath)) {
-		return false;
-	}
-	$resultValue = file($filePath);
-	if (count($resultValue)) {
-		return $resultValue;
-	}
-	return false;
 }
--- src/index.php
+++ src/index.php
@@ -2,21 +2,20 @@
 require_once __DIR__ . '/../functions/functions.php';
 
 $path = '../log.txt';
-echo 'Введіть аргументи в консоль' . PHP_EOL;
-$resultWrite = getConsole1($path);
-if($resultWrite !== false){
-	echo 'Інформація успішно записана в файл log.txt' . PHP_EOL;
-} else{
-	echo 'Інформація не записана в файл log.txt' . PHP_EOL;
+$allGetConsole = true;
+while($allGetConsole) {
+	echo 'Введіть аргументи в консоль або залиште поле пустим, щоб закінчити програму' . PHP_EOL;
+	$allGetConsole = getConsole();
+	if (!$allGetConsole) {
+		continue;
+	}
+	$resultWrite = writeFile($path, $allGetConsole);
+	if($resultWrite !== false){
+		echo 'Інформація успішно записана в файл log.txt' . PHP_EOL;
+	} else{
+		echo 'Інформація не записана в файл log.txt' . PHP_EOL;
+	}
 }
-
-echo 'Варіант 2. Введіть аргументи в консоль' . PHP_EOL;
-$results = getConsole2($path);
-if($results === false){
-	echo 'Інформація не записана в файл log.txt' . PHP_EOL;
-}
-	echo 'Інформація успішно записана в файл log.txt' . PHP_EOL;
-
 
 $result = '';
 foreach (fileReader('../log.txt') as $line){
@@ -24,11 +23,3 @@ foreach (fileReader('../log.txt') as $line){
 }
 echo 'Останні аргументи попередньої програми, які були введені - ' . $result . PHP_EOL ;
 
-$filePath2 = '../log.txt';
-$resultReade2 = fileReade2($filePath2);
-if (is_array($resultReade2)) {
-	$getValue = end($resultReade2);
-	echo 'Останні аргументи попередньої програми, які були введені - ' . $getValue . PHP_EOL ;
-} else {
-	echo 'Файл порожній' . PHP_EOL;
-}
