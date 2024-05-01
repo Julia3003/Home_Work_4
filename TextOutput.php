0de96cb082a2433724d08e50c9d09524a1e71ae7
Julia Korchevskaja <yourii.korchevskii@forforce.com>
2024-05-01 21:54:20 +0300

    Home_work_14

---
+++ TextOutput.php
@@ -0,0 +1,34 @@
+<?php
+class TextOutput
+{
+	private string $text = "some text";
+	
+	
+	public function __construct(string $text)
+	{
+		$this->setText();
+	}
+	
+	/**
+	 * @param string $text
+	 */
+	public function setText(string $text): void
+	{
+		$this->text = $text;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getText(): string
+	{
+		return $this->text;
+	}
+	
+	
+	function print(): void
+	{
+		$uppercase = ucfirst($this->text);
+	}
+	
+}
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/BankAccount.php';
+require_once __DIR__ . '/TextOutput.php';
 
 $topUpAmount = 50;
 try {
