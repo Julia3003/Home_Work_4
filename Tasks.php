b59817ca8e520f9e9945f4898f43ba04b036116e
Julia Korchevskaja <yourii.korchevskii@forforce.com>
2024-05-03 19:59:59 +0300

    Home_work_13

---
+++ Tasks.php
@@ -0,0 +1,105 @@
+<?php
+
+class Tasks
+{
+	protected string $taskName;
+	protected int $priority;
+	
+	protected string $fileName = 'tasks.txt';
+	
+	
+	/**
+	 * @param string $taskName
+	 * @return void
+	 */
+	public function setTaskName(string $taskName): void
+	{
+		if (strlen($taskName) < 5) {
+			throw new Exception(message: 'Task name consists of less than 5 characters');
+		}
+		$this->taskName = $taskName;
+	}
+	
+	/**
+	 * @param int $priority
+	 * @return void
+	 */
+	public function setPriority(int $priority): void
+	{
+		if ($priority <= 0 && $priority >= 4) {
+			throw new Exception(message: 'The priority can only have a value of 1,2,3:
+				- 1 - high priority;
+				- 2 - medium priority;
+				- 3 - low priority.'
+			);
+		}
+		$this->priority = $priority;
+	}
+	
+	/**
+	 * @param string $fileName
+	 */
+	public function setFileName(string $fileName): void
+	{
+		$this->fileName = $fileName;
+	}
+	
+	
+	/**
+	 * @return string
+	 */
+	public function getTaskName(): string
+	{
+		return $this->taskName;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getPriority(): int
+	{
+		return $this->priority;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getFileName(): string
+	{
+		return $this->fileName;
+	}
+	
+	public function addTask($taskName, $priority): bool
+	{
+		$taskId = uniqid();
+		$fileName = $this->getFileName();
+		return (bool)file_put_contents($fileName, "$taskId|$taskName|$priority\n", FILE_APPEND);
+	}
+	
+	public function getTasks(): array
+	{
+		$tasks = file($this->getFileName());
+		$taskArray = [];
+		foreach ($tasks as $task) {
+			$taskArray[] = explode('|', $task);
+		}
+		usort($taskArray, function ($a, $b) {
+			return $b[2] <=> $a[2];
+		});
+		return $taskArray;
+		
+	}
+	public function deleteTask (string $taskId): array
+	{
+		$tasks = array_filter($this->getTasks(), function ($task) use ($taskId) {
+			return $task[0] !== $taskId;
+		});
+		$tasksString = '';
+		foreach ($tasks as $task) {
+			$tasksString .= implode('|', $task);
+		}
+		
+		file_put_contents($this->getFileName(), $tasksString);
+		return $tasks;
+	}
+}
--- index.php
+++ index.php
@@ -2,7 +2,22 @@
 
 require_once __DIR__ . '/MainText.php';
 require_once __DIR__ . '/Text.php';
+require_once __DIR__ . '/Tasks.php';
 require_once __DIR__ . '/function/functions.php';
+
+$tasks = new Tasks();
+//$taskList = $tasks->getTasks();
+////var_dump($taskList);
+//
+//if ($tasks->addTask('Good deer', 3)) {
+//	echo 'Завдання успішно додано в файл' . PHP_EOL;
+//} else {
+//	echo 'Завдання не додано' . PHP_EOL;
+//}
+$deleteTask = $tasks->deleteTask('66351163aef17');
+//var_dump($deleteTask);
+
+
 
 //create
 //$taskId = uniqid();
@@ -11,30 +26,32 @@ require_once __DIR__ . '/function/functions.php';
 //file_put_contents('tasks.txt', "$taskId|$taskName|$priority\n", FILE_APPEND);
 
 //read
-$tasks = file('tasks.txt');
-foreach ($tasks as $key => $task) {
-	$task = explode('|', $task);
-	array_walk($task, function (&$value) {
-		$value = trim($value);
-	});
-	$tasks[$key] = $task;
-}
-
-
-
+//$tasks = file('tasks.txt');
+//foreach ($tasks as $key => $task) {
+//	$task = explode('|', $task);
+//	array_walk($task, function (&$value) {
+//		$value = trim($value);
+//	});
+//	$tasks[$key] = $task;
+//}
+//
+//
+//
 //deleteTask($taskId)
-$taskId = '6633a87dbb286';
-$tasks = array_filter($tasks, function ($task) use ($taskId)
-{
-	return $task[0] !== $taskId;
-});
-print_r($tasks);
+//$taskId = '6633a87dbb286';
+//$tasks = array_filter($tasks, function ($task) use ($taskId)
+//{
+//	return $task[0] !== $taskId;
+//});
+////print_r($tasks);
+//
+////write
+//unlink('tasks.txt');
+//foreach ($tasks as $task) {
+//	file_put_contents('tasks.txt', "$task[0]|$task[1]|$task[2]\n",FILE_APPEND);
+//}
 
-//write
-unlink('tasks.txt');
-foreach ($tasks as $task) {
-	file_put_contents('tasks.txt', "$task[0]|$task[1]|$task[2]\n",FILE_APPEND);
-}
+
 
 //try {
 //	$mainText = new MainText();
