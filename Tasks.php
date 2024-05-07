caa03967564d05199a88a5d7e78dd7b8d28a64db
Julia Korchevskaja <yourii.korchevskii@forforce.com>
2024-05-07 17:12:55 +0300

    Home_work_13_hw

--- Tasks.php
+++ Tasks.php
@@ -2,12 +2,7 @@
 
 class Tasks
 {
-	protected string $taskName;
-	
-	protected int $priority;
-	
 	protected string $fileName;
-//	protected TaskState $stateTask = TaskState::NotDone;
 	
 	protected array $tasksList;
 	
@@ -21,28 +16,26 @@ class Tasks
 	 * @param string $taskName
 	 * @return void
 	 */
-	public function setTaskName(string $taskName): void
+	public function validateTaskName(string $taskName): void
 	{
 		if (strlen($taskName) < 5) {
 			throw new Exception(message: 'Task name consists of less than 5 characters');
 		}
-		$this->taskName = $taskName;
 	}
 	
 	/**
 	 * @param int $priority
 	 * @return void
 	 */
-	public function setPriority(int $priority): void
+	public function validatePriority(int $priority): void
 	{
-		if ($priority <= 0 && $priority >= 4) {
+		if ($priority < 1 || $priority > 3) {
 			throw new Exception(message: 'The priority can only have a value of 1,2,3:
 				- 1 - high priority;
 				- 2 - medium priority;
 				- 3 - low priority.'
 			);
 		}
-		$this->priority = $priority;
 	}
 	
 	/**
@@ -51,31 +44,6 @@ class Tasks
 	public function setFileName(string $fileName): void
 	{
 		$this->fileName = $fileName;
-	}
-	
-	/**
-	 * @param TaskState $stateTask
-	 */
-	public function setStateTask(TaskState $stateTask): void
-	{
-		$this->stateTask = $stateTask;
-	}
-	
-	
-	/**
-	 * @return string
-	 */
-	public function getTaskName(): string
-	{
-		return $this->taskName;
-	}
-	
-	/**
-	 * @return int
-	 */
-	public function getPriority(): int
-	{
-		return $this->priority;
 	}
 	
 	/**
@@ -94,19 +62,10 @@ class Tasks
 		return $this->tasksList;
 	}
 	
-//	/**
-//	 * @return TaskState
-//	 */
-//	public function getStateTask(): TaskState
-//	{
-//		return $this->stateTask;
-//	}
-	
-	public function addTask($taskName, $priority, $stateTask = TaskState::NotDone): void
+	public function addTask(string $taskName, int $priority, TaskState $stateTask = TaskState::NotDone): void
 	{
-		if ($priority < 1 || $priority > 3) {
-			throw new Exception('Некоректно вказано пріорититет');
-		}
+		$this->validatePriority($priority);
+		$this->validateTaskName($taskName);
 
 		$taskId = uniqid();
 		$this->tasksList[$taskId] = [
@@ -151,15 +110,10 @@ class Tasks
 		file_put_contents($this->fileName, $fileData);
     }
 
-//	public function completeTask($taskId): bool
-//	{
-//		$tasks = $this->getTasks();
-//		foreach ($tasks as $key => $task) {
-//			if($task[0] == $taskId) {
-//				$tasks[$key][3] = TaskState::Done->value . PHP_EOL;
-//			}
-//		}
-//		return $this->saveTaskInFile($tasks);
-//	}
-
+	public function completeTask($taskId)
+	{
+		if (!array_key_exists($taskId, $this->tasksList)) {
+			throw new Exception('Завдання з вказаним id' . $taskId . 'не знайдено');
+		} $this->tasksList[$taskId][2] = TaskState::Done->value . PHP_EOL;
+	}
 }
