f89669b0154816bb0e79c4e92ff64ed1248b7179
Julia Korchevskaja <yourii.korchevskii@forforce.com>
2024-05-08 22:30:33 +0300

    Home_work_13_hw

--- function/functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-function showText(MainText $newText): void
-{
-	echo $newText->print() . PHP_EOL;
-}
--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
 
-require_once __DIR__ . '/MainText.php';
-require_once __DIR__ . '/Text.php';
 require_once __DIR__ . '/Tasks.php';
 require_once __DIR__ . '/TaskState.php';
-require_once __DIR__ . '/function/functions.php';
 
-//print_r(TaskState::cases());
+
 $fileName = 'tasks.txt';
 $tasks = new Tasks($fileName);
 $taskList = $tasks->getTasksList();
-//var_dump($taskList);
+
 
 try {
 	$tasks->addTask('Bad day', 3);
@@ -26,51 +23,9 @@ try {
 	echo 'По Завданню ' . $taskId . ' статус не змінено' . PHP_EOL;
 }
 
-//$deleteTask = $tasks->deleteTask('66351163aef17');
+try {
+	$tasks->deleteTask('66351163aef17');
+} catch (Exception $exception) {
+	echo $exception->getMessage() . PHP_EOL;
+}
 
-
-
-
-//create
-//$taskId = uniqid();
-//$taskName = 'Cool task';
-//$priority = 2;
-//file_put_contents('tasks.txt', "$taskId|$taskName|$priority\n", FILE_APPEND);
-
-//read
-//$tasks = file('tasks.txt');
-//foreach ($tasks as $key => $task) {
-//	$task = explode('|', $task);
-//	array_walk($task, function (&$value) {
-//		$value = trim($value);
-//	});
-//	$tasks[$key] = $task;
-//}
-//
-//
-//
-//deleteTask($taskId)
-//$taskId = '6633a87dbb286';
-//$tasks = array_filter($tasks, function ($task) use ($taskId)
-//{
-//	return $task[0] !== $taskId;
-//});
-////print_r($tasks);
-//
-////write
-//unlink('tasks.txt');
-//foreach ($tasks as $task) {
-//	file_put_contents('tasks.txt', "$task[0]|$task[1]|$task[2]\n",FILE_APPEND);
-//}
-
-
-
-//try {
-//	$mainText = new MainText();
-//	showText($mainText);
-//
-//	$newText = new Text();
-//	showText($newText);
-//} catch (Exception $exception) {
-//	echo $exception->getMessage() . PHP_EOL;
-//}
