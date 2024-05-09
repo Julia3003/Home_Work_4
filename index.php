4ef5916159171b4846c1a4a0594b5d7bddc48d3b
Julia Korchevskaja <yourii.korchevskii@forforce.com>
2024-05-09 21:52:53 +0300

    Home_work_13_hw

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ try {
 } catch (Exception $exception) {
 	echo 'Завдання не додано. Помилка ' . $exception->getMessage() . PHP_EOL;
 }
-$taskId = '663bd06ac525c';
+$taskId = '663a3fa078db5';
 try {
 	$tasks->completeTask($taskId);
 	echo 'По Завданню ' . $taskId . ' статус змінено на ' . TaskState::Done->value . PHP_EOL;
@@ -23,7 +23,7 @@ try {
 	echo 'По Завданню ' . $taskId . ' статус не змінено' . PHP_EOL;
 }
 
-$taskId = '663bd9041522f';
+$taskId = '663d176e19bb8';
 try {
 	$tasks->deleteTask($taskId);
 	echo 'Завдання ' . $taskId . ' успішно видалено' . PHP_EOL;
