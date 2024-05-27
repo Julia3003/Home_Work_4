f0d1df6805298dae77f2e216ec17bf727839d734
Julia Korchevskaja <yourii.korchevskii@forforce.com>
2024-05-27 19:05:22 +0300

    Home_work_20

--- index.php
+++ index.php
@@ -4,3 +4,18 @@ define('APP_DIR', __DIR__ . 'index.php/');
 
 require_once 'system/Config.php';
 require_once 'controllers/FrontController.php';
+require_once 'system/Request.php';
+
+$router = new Router();
+
+$router->addRoute('/front', [
+	'get' => 'FrontController@hello',
+	'post' => 'FrontController@sum',
+]);
+
+//$router->addRoute('/login', [
+//	'get' => 'AuthController@login',
+//	'post' => 'AuthController@auth',
+//]);
+
+$router->processRoute(Request::getUrl(), Request::getMethod());
